3d6f8ad1af80b11e65773b7d55371937702dc4bb
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-06-17 18:24:39 -0400

    Updated JS
    *JS*
    
    - added prompts and validation for prompts.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  // Display Prompts
+  var passLength = prompt('Password length: (MUST be a minimum 8 but no more than 128)', '8');
+  
+  // Check if passLength is valid, meaning its 8 or more, 128 or less and is a number 
+  if(passLength < 8 || passLength > 128 || isNaN(passLength)){
+    var errorMsg = alert('Password length is invalid or something other than a whole number was entered, please try again');
+  }
+  // If the passLength is valid then proceed with the rest of the prompts
+  else{
+  
+  // In order from top to bottom, ask user if they want to include: lowercase letters, UPPERCASE LETTERS, Numbers, Special Characters
+  var passLowercase = confirm('Include lowercase letters?\nOk = Yes\nCancel = No');
+  var passUppercase = confirm('Include uppercase letters?\nOk = Yes\nCancel = No');
+  var passNumbers = confirm('Include Numbers?\nOk = Yes\nCancel = No');
+  var passSpecialCharacters = confirm('Include Special Characters?\nOk = Yes\nCancel = No');
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
