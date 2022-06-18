c04c2f6a5d870047e4132d840de9a52bf596b7b9
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-06-18 18:29:38 -0400

    Update JS
    *JS*
    
    - Added code to alert that user that no password criteria was selected
    
    - Added code to show preserve placeholder text if no password has been generated

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passWasGenerated;
 
 // ******** from W3DOCS: https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
 function shuffleArray(array) {
@@ -17,24 +18,23 @@ function shuffleArray(array) {
   return array;
 }
 
-// ******** from MDN WEB DOCS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// ******** from MDN reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 // =====================================  Write password to the #password input
 function writePassword() {
-  
+  passWasGenerated = false;
   // Display Prompts
   var passLength = prompt('Password length: (MUST be a minimum 8 but no more than 128)', '8');
   
   // Check if passLength is valid, meaning its 8 or more, 128 or less and is a number 
   if(passLength < 8 || passLength > 128 || isNaN(passLength)){
-    var errorMsg = alert('Password length is invalid or something other than a whole number was entered, please try again');
+    alert('Password length is invalid or something other than a whole number was entered, please try again');
   }
   // If the passLength is valid then proceed with the rest of the prompts
   else{
-  
   // In order from top to bottom, ask user if they want to include: lowercase letters, UPPERCASE LETTERS, Numbers, Special Characters
   var passLowercase = confirm('Include lowercase letters?\nOk = Yes\nCancel = No');
   var passUppercase = confirm('Include uppercase letters?\nOk = Yes\nCancel = No');
@@ -92,11 +92,15 @@ function writePassword() {
       return myArrayOfCriteria;
   }
 
-
   // Stores compiled password criteria as a variable
   var selectedCriteria = gatherSelectedCriteria();
 
-
+  // Check if user selected any criteria, if none are selected show alert. If there is at least one criteria selected the generated the password
+  if(selectedCriteria.length === 0){
+    alert("No options were chosen. Please try again.");
+    return;
+  }
+  else{
   // ========================= Generates characters based on selected Criteria
   function buildPassword(criteria){
     var myCharacter;
@@ -104,7 +108,6 @@ function writePassword() {
       case 'lower':
         myCharacter = randomLetterGenerator();
         break;
-      
       case 'upper':
         myCharacter = randomLetterGenerator(true);
         break;
@@ -115,12 +118,12 @@ function writePassword() {
         myCharacter = randomSpecialCharacterGenerator();
         break;
         default:
-        console.log("Something went terribly wrong if you're seeing this!");
-    }
+          console.log("Default Case hit")
+      }
     return myCharacter;
   }
 
-  //  ======================== Preloads generatedpass array with one of each type of selected criteria
+  //  ======================== Preloads generatedPass array with one of each type of selected criteria
   function preloadGeneratedPass() {
     for(var i = 0; i < selectedCriteria.length; i++){
       generatedPass.push(buildPassword(selectedCriteria[i]));
@@ -128,8 +131,8 @@ function writePassword() {
     return generatedPass;
   }
 
-
   preloadGeneratedPass();
+
   // Generates the rest of password criteria at random
   for (var i = 0; i < length - selectedCriteria.length; i++){
     generatedPass.push(buildPassword(selectedCriteria[getRandomInt(selectedCriteria.length)]));
@@ -138,16 +141,21 @@ function writePassword() {
 
   // generatedPass.join('');
   var shuffledPass = shuffleArray(generatedPass);
+  passWasGenerated = true;
   return shuffledPass.join(''); 
   }
 
+}
 
   // Call generatePassword, store it and update the HTML element so the user can see their generated password
   var password = generatePassword(passLength, passLowercase, passUppercase, passNumbers, passSpecialCharacters);
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  
+  // Check if password was generated, if not leave existing text, if it was then update the html element with the generated password
+  if(passWasGenerated){
+    passwordText.value = password;
+  }
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
