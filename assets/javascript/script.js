efe572ddc35ab1b534a8f4d7c4ecf531f0cf4188
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-06-17 21:57:08 -0400

    Updated JS
    *JS*
    
    - Added code from W3DOCS for shuffling an array.
    
    - Commented additional functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// from W3DOCS: https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
+function shuffleArray(array) {
+  var curId = array.length;
+  // There remain elements to shuffle
+  while (0 !== curId) {
+    // Pick a remaining element
+    var randId = Math.floor(Math.random() * curId);
+    curId -= 1;
+    // Swap it with the current element.
+    var tmp = array[curId];
+    array[curId] = array[randId];
+    array[randId] = tmp;
+  }
+  return array;
+}
+// from MDN WEB DOCS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 // Write password to the #password input
 function writePassword() {
-
+  
   // Display Prompts
   var passLength = prompt('Password length: (MUST be a minimum 8 but no more than 128)', '8');
   
@@ -48,65 +67,71 @@ function writePassword() {
     
     return mySpecialCharacter; 
   }
-}
+  // Compiles selected password criteria into an array.
+  function gatherSelectedCriteria(){
+      var myArrayOfCriteria = [];
+      if(passLowercase){
+        myArrayOfCriteria.push('lower');
+      }
+      
+      if(passUppercase) {
+        myArrayOfCriteria.push('upper');
+      }
+      if(passNumbers) {
+        myArrayOfCriteria.push('num');
+      }
+      if(passSpecialCharacters){
+        myArrayOfCriteria.push('special');
+      }
+      
+      return myArrayOfCriteria;
+  }
+  // Stores compiled password criteria as a variable
+  var selectedCriteria = gatherSelectedCriteria();
+  // Generates characters based on selected Criteria
+  function buildPassword(criteria){
+    var myCharacter;
+    switch (criteria){
+      case 'lower':
+        myCharacter = randomLetterGenerator();
+        break;
+      
+      case 'upper':
+        myCharacter = randomLetterGenerator(true);
+        break;
+      case 'num':
+        myCharacter = randomNumberGenerator();
+        break;
+      case 'special':
+        myCharacter = randomSpecialCharacterGenerator();
+        break;
+        default:
+        console.log("Something went terribly wrong if you're seeing this!");
+    }
+    return myCharacter;
+  }
+  //  Preloads generatedpass array with one of each type of selected criteria
+  function preloadGeneratedPass() {
+    for(var i = 0; i < selectedCriteria.length; i++){
+      generatedPass.push(buildPassword(selectedCriteria[i]));
+    }
+    return generatedPass;
+  }
+  preloadGeneratedPass();
+  // Generates the rest of password criteria at random
+  for (var i = 0; i < length - selectedCriteria.length; i++){
+    generatedPass.push(buildPassword(selectedCriteria[getRandomInt(selectedCriteria.length)]));
+  } 
+  
+  // generatedPass.join('');
+  var shuffledPass = shuffleArray(generatedPass);
+  return shuffledPass.join(''); 
+  }
 
-// Compiles selected password criteria into an array.
-function gatherSelectedCriteria(){
-  var myArrayOfCriteria = [];
-  if(passLowercase){
-    myArrayOfCriteria.push('lower');
-  }
-  
-  if(passUppercase) {
-    myArrayOfCriteria.push('upper');
-  }
-  if(passNumbers) {
-    myArrayOfCriteria.push('num');
-  }
-  if(passSpecialCharacters){
-    myArrayOfCriteria.push('special');
-  }
-  
-  return myArrayOfCriteria;
-}
-// Stores compiled password criteria as a variable
-var selectedCriteria = gatherSelectedCriteria();
-// Generates characters based on selected Criteria
-function buildPassword(criteria){
-var myCharacter;
-switch (criteria){
-  case 'lower':
-    myCharacter = randomLetterGenerator();
-    break;
-  
-  case 'upper':
-    myCharacter = randomLetterGenerator(true);
-    break;
-  case 'num':
-    myCharacter = randomNumberGenerator();
-    break;
-  case 'special':
-    myCharacter = randomSpecialCharacterGenerator();
-    break;
-    default:
-    console.log("Something went terribly wrong if you're seeing this!");
-}
-return myCharacter;
-}
-//  Preloads generatedpass array with one of each type of selected criteria
-function preloadGeneratedPass() {
-for(var i = 0; i < selectedCriteria.length; i++){
-  generatedPass.push(buildPassword(selectedCriteria[i]));
-}
-return generatedPass;
-}
-
-  var password = generatePassword();
+  // Call generatePassword, store it and update the HTML element so the user can see their generated password
+  var password = generatePassword(passLength, passLowercase, passUppercase, passNumbers, passSpecialCharacters);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
